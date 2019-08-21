7ddfacbb63afbc188491e53321e41ce78babfc27
Matt <matthew.payne1988@gmail.com>
2019-08-22 11:58:56 +1200

    Entire Project

---
+++ Assets/Scripts/DialogueDescription.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//This allows us to the make the scriptable objects in the project hierarchy through right clicking
+[CreateAssetMenu(fileName ="New DialogueDescription", menuName = "Dialogue Description")]
+public class DialogueDescription : ScriptableObject {
+
+    //We need a blank sprite so we can clearly show who is talking by 
+    //turning the non talking character 'Off'
+    public Sprite blankSprite;
+
+    [Header("Dialogue")]
+    //We take the dialogue struct (collection of data) and stick '[]' at the end
+    //to turn it into an array (an array is like a shoping list)
+    public Dialogue[] dialogue;
+}
+
+public enum CharacterType
+{
+    //Enums allow us to store data and assign it a value
+    Player = 0,
+    NPC = 1,
+}
+
+public enum CharacterList
+{
+    //Enums allow us to store data and assign it a value but in this case
+    //we dont need it to as we are going to retrieve the character names
+    Pheonix,
+    Gumshoe
+}
+
+[System.Serializable]
+public struct Dialogue
+{
+    //This struct will allow us to collect a load of data so that out dialogue system 
+    //can give us the performance we need
+    public Sprite characterSprite;
+    public CharacterType characterType;
+    public CharacterList name;
+    [TextArea(0, 800)]
+    public string dialogue;
+}
---
+++ Assets/Scripts/DialogueSystem.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class DialogueSystem : MonoBehaviour {
+
+    [Header("Dialogue Description")]
+    //Here is where we are going to slot in our dialogue descriptions
+    public DialogueDescription description;
+
+    [Header("UI Components")]
+    //Here we want to gather all the UI Components so we are able
+    //to manipulate them via code
+    public Text dialogueName;
+    public Text dialogueText;
+    public Image pcImage;
+    public Image npcImage;
+
+    //These 'private' functions allow us to pass data between functions with in the script
+    //setting them to private stop the functions from being manipulated externaly either by 
+    //editing the values in the Unity Inspector or by another script 
+    private int dialogueIndex = -1;
+    private bool isFinished = false;
+
+    private void Start()
+    {
+        StartEndDialogue();
+    }
+
+    private void LateUpdate()
+    {
+        //Check if the player is has pressed the space key and that the dialogue index number is less than
+        //the length of the array
+        if (Input.GetKeyDown(KeyCode.Space) && dialogueIndex < description.dialogue.Length)
+        {
+            CycleDialogue();
+        }
+
+        if (isFinished)
+        {
+            StartEndDialogue();
+        }     
+    }
+
+    void CycleDialogue()
+    {
+        //This line of code allows us to add '1' to its self (basicly: dialogueIndex + 1) 
+        dialogueIndex++;
+
+        if (dialogueIndex < description.dialogue.Length)
+        {
+            //Here we are taking the names of the characters and what the dialogue from 
+            //the enums in the scriptable object & converting it to a string (text) 
+            dialogueName.text = description.dialogue[dialogueIndex].name.ToString();
+            dialogueText.text = description.dialogue[dialogueIndex].dialogue.ToString();
+
+            //Here we are taking the character sprites from the scriptable object and passing them
+            //the UI 
+            pcImage.sprite = description.dialogue[dialogueIndex].characterSprite;
+            npcImage.sprite = description.dialogue[dialogueIndex].characterSprite;
+
+            SetBlankSprite();
+        }
+        else
+        {
+            //Here we are changing the isFinished bool to make the dialogue system 
+            //stop cycling through the dialogue
+            isFinished = true;
+        }        
+    }
+
+    void SetBlankSprite()
+    {
+        //Toggle between player and npc sprite while talking 
+        if (description.dialogue[dialogueIndex].characterType != 0)
+        {
+            pcImage.sprite = description.blankSprite;
+        }
+        else
+        {
+            npcImage.sprite = description.blankSprite;
+        }
+    }
+
+    void StartEndDialogue()
+    {
+        //Set both player and npc sprites blank
+        pcImage.sprite = description.blankSprite;
+        npcImage.sprite = description.blankSprite;
+
+        //Check to see if dialogue index is not the same value as the length of dialogue
+        // and also is the story finished
+        if (dialogueIndex != description.dialogue.Length -1 && isFinished == false)
+        {
+            dialogueName.text = "";
+            dialogueText.text = "Press Space to start";
+        }
+        else
+        {
+            dialogueName.text = "";
+            dialogueText.text = "The End";
+        }
+    }
+}
